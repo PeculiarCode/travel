1051e0401f72433a51a484c39aee75ebed781f27
PeculiarCode <tunshisky@outlook.com>
2020-03-15 10:04:50 +0800

    header

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="com-wrapper">
-      hello world
-  </div>
+    <div>
+        <home-header></home-header>
+    </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header';
 export default {
-  data() {
-    return {};
-  },
-  created() {},
-  mounted() {},
-  methods: {},
-  computed: {},
-  components: {}
+    data() {
+        return {};
+    },
+
+    created() {},
+    mounted() {},
+    methods: {},
+    computed: {},
+    components: {
+        HomeHeader
+    }
 };
 </script>
 <style lang="stylus" scoped></style>
