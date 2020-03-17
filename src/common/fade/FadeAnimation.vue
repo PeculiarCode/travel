cf715646c771df6e18356a943b9a821d30fea917
PeculiarCode <tunshisky@outlook.com>
2020-03-17 14:36:17 +0800

    fade-animation

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <transition name="fade">
+            <slot></slot>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    created() {},
+    mounted() {},
+    methods: {},
+    computed: {},
+    components: {}
+};
+</script>
+<style lang="stylus" scoped>
+.fade-enter,.fade-leave-to
+    opacity 0
+.fade-enter-active,.fade-leave-active
+    transition opacity .7s
+</style>
