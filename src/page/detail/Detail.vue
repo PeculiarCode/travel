c293203d6cddd5fed0fbed2b1d999c596a8550a6
PeculiarCode <tunshisky@outlook.com>
2020-03-17 12:32:21 +0800

    detail-header

--- src/page/detail/Detail.vue
+++ src/page/detail/Detail.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
-import DetailBanner from './components/Banner'
+import DetailBanner from './components/Banner';
+import DetailHeader from './components/Header';
 export default {
     data() {
         return {};
@@ -15,8 +18,12 @@ export default {
     methods: {},
     computed: {},
     components: {
-        DetailBanner
+        DetailBanner,
+        DetailHeader
     }
 };
 </script>
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.content
+    height 50rem
+</style>
---
+++ src/page/detail/components/Header.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+            <div class="iconfont header-abs-back">&#xe624;</div>
+        </router-link>
+        <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+            <router-link to="/">
+                <div class="iconfont header-fixed-back">&#xe624;</div>
+            </router-link>
+            <span>景点详情</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DetailHeader',
+    data() {
+        return {
+            showAbs: true,
+            opacityStyle: {
+                opacity: 0
+            }
+        };
+    },
+    computed: {},
+    methods: {
+        handleScroll() {
+            const top = document.documentElement.scrollTop;
+            if (top > 10) {
+                let opacity = top / 140;
+                opacity = opacity > 1 ? 1 : opacity;
+                this.opacityStyle = { opacity };
+                //对象let obj = { key : value },key和value相等的话就简写
+                // this.opacityStyle.opacity = opacity;
+                // this.opacityStyle = {
+                //     opaciry: opacity
+                // };
+                this.showAbs = false;
+            } else {
+                this.showAbs = true;
+            }
+        }
+    },
+    activated() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    deactivated() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header-abs
+  position: absolute
+  left: .2rem
+  top: .4rem
+  width: .8rem
+  height: .8rem
+  line-height: .8rem
+  border-radius: .4rem
+  text-align: center
+  background: rgba(0, 0, 0, .8)
+  .header-abs-back
+    color: #fff
+    font-size: .4rem
+
+.header-fixed
+  position: fixed
+  top: 0
+  left: 0
+  right: 0
+  height: $headerHeight
+  line-height: $headerHeight
+  text-align: center
+  color: #fff
+  background: $bgColor
+  font-size: .32rem
+  .active
+        color red
+  .header-fixed-back
+    position: absolute
+    top: 0
+    left: 0
+    width: .64rem
+    text-align: center
+    font-size: .4rem
+    color: #fff
+</style>
